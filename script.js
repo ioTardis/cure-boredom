cf7c5e96e563d872a6794aeb935fed1453ab6930
Izabella Golovina <ia.golovina00@gmail.com>
2022-06-19 18:14:40 +0300

    minor refactoring

--- script.js
+++ script.js
@@ -3,35 +3,47 @@ const sendBtn = document.querySelector('#send-request');
 sendBtn.addEventListener('click', getActivity);
 
 function getActivity() {
-    let url = 'http://www.boredapi.com/api/activity/';
+    const keyField = document.querySelector('#key').value;
+    console.log(keyField);
+    if (keyField != 0) {
+        const api = 'http://www.boredapi.com/api/activity/';
+        let url = api + '?key=' + keyField;
     fetch(url)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            const activityName = document.querySelector('#activity-name');
-            const activityKey = document.querySelector('#activity-key');
-            const activityType = document.querySelector('#activity-type');
-            const activityPart = document.querySelector('#activity-participants');
-            const activityPrice = document.querySelector('#activity-price');
-            const activityLink = document.querySelector('#activity-link');
-            const activityAccess = document.querySelector('#activity-accessibility');
- 
-            activityName.textContent = data['activity'];
-            activityKey.textContent = data['key'];
-            activityType.textContent = `Activity type: ${data['type']}`;
-            activityPart.textContent = `Number of participants: ${data['participants']}`;
-            activityPrice.textContent = `Activity price: ${data['price']}`;
-            if (data['link'] != '') {
-                activityLink.textContent = '';
-                const a = document.createElement('a');
-                a.setAttribute('href', data['link']);
-                a.textContent = 'Link';
-                activityLink.appendChild(a);
-            } else activityLink.textContent = 'No link';
-            activityAccess.textContent = `Activity accessibility: ${data['accessibility']}`;
+            displayActivity(data);
+            console.log(data);
         })
         .catch(function(error) {
             console.log(error);
         });
+    }
+}
+
+function displayActivity(data) {
+    // const activityDiv = document.querySelector('.activity');
+    // if (data['error'] != 0) activityDiv.textContent = 'No activity with this key';
+    const activityName = document.querySelector('#activity-name');
+    const activityKey = document.querySelector('#activity-key');
+    const activityType = document.querySelector('#activity-type');
+    const activityPart = document.querySelector('#activity-participants');
+    const activityPrice = document.querySelector('#activity-price');
+    const activityLink = document.querySelector('#activity-link');
+    const activityAccess = document.querySelector('#activity-accessibility');
+ 
+    activityName.textContent = data['activity'];
+    activityKey.textContent = data['key'];
+    activityType.textContent = `Activity type: ${data['type']}`;
+    activityPart.textContent = `Number of participants: ${data['participants']}`;
+    activityPrice.textContent = `Activity price: ${data['price']}`;
+    if (data['link'] != '') {
+        activityLink.textContent = '';
+        const a = document.createElement('a');
+        a.setAttribute('href', data['link']);
+        a.textContent = 'Link';
+        activityLink.appendChild(a);
+    } else activityLink.textContent = 'No link';
+    activityAccess.textContent = `Activity accessibility: ${data['accessibility']}`;
 }
